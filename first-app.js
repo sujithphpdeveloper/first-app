1015bac2ccfecc9f06640504bda3fe72c84e4a78
Sujith HOC <sujith@houseofcomms.com>
2023-10-19 14:35:58 +0400

    Start with the basic setup and javascript introduction tests

---
+++ first-app.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.writeFileSync('hello.text', 'hello from the first app');
---
+++ play.js
@@ -0,0 +1,8 @@
+let name = "sujith";
+let age = '30';
+
+const summarizeUser = (userName,  userAge) => {
+    return 'The user ' + userName + ' is with age '+ userAge;
+}
+
+console.log(summarizeUser(name, age));
