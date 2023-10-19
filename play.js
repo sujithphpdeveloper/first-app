c8d04a128e00fdb9f725d24b27eae67b68e17089
Sujith HOC <sujith@houseofcomms.com>
2023-10-19 15:15:19 +0400

    Rest & Spread

--- play.js
+++ play.js
@@ -6,4 +6,16 @@ const person = {
     }
 };
 
-person.greet();
+const hobbies = ['football', 'parkour', 'drive'];
+hobbies.push('Programming');
+//console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+//console.log(hobbies)
+
+//Spread Operator - Copy/Move Array
+const copiedArray = [...hobbies];
+
+// Rest do action on each element
+const copiedRestArray = (...args) => {
+    return args;
+}
+console.log(copiedRestArray(1,2,3,4,4))
